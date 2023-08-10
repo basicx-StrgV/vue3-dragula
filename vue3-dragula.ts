ff69380f71d4420ccc874a104a8595fedce57522
Bjarne <59922570+basicx-StrgV@users.noreply.github.com>
2023-08-10 20:12:58 +0200

    Added type enforcement; Added VueDragulaGlobal class to replace the old implementation of injecting data into the global object; Data structure changes; Additional small improvements; Code cleanup

--- vue3-dragula.ts
+++ vue3-dragula.ts
@@ -8,127 +8,158 @@
 //--------------------------------------------------//
 
 import dragula from 'dragula';
+import type { Drake, DragulaOptions } from 'dragula';
 import mitt from 'mitt';
 import type { Emitter } from 'mitt';
+import { nextTick } from 'vue';
 import type { App, Directive, DirectiveBinding, VNode } from 'vue';
 
 if (!dragula) {
-    throw new Error('[vue3-dragula] cannot locate dragula.');
+    throw new Error('[vue3-dragula] cannot locate package: dragula');
 }
 
-export default function(vueApp: App) {
-    const service = new DragulaService();
+if (!mitt) {
+  throw new Error('[vue3-dragula] cannot locate package: mitt');
+}
+
+export class Bag {
+  name: string;
+  drake: Drake | null;
+  drakeRegistert: boolean = false;
+  initEvents: boolean = false;
+  models: any[] = [];
+
+  constructor(name: string, drake: any){
+    this.name = name;
+    this.drake = drake;
+  }
+
+  destroy(){
+    this.drakeRegistert = false;
+    this.initEvents = false;
+    this.models.splice(0, this.models.length);
+    if(this.drake != null){
+      this.drake.destroy();
+    }
+    this.drake = null;
+  }
+}
+
+export class VueDragulaGlobal {
+  static eventBus: Emitter<any>;
+  static options: Function;
+  static find: Function;
+}
+
+export function vueDragula(vueApp: App) {
+    const service: DragulaService = new DragulaService();
 
     let name: string = 'globalBag';
-    let drake: any;
+    let drake: Drake | null;
 
-    (globalThis as any).vueDragula = {
-        options: service.setOptions.bind(service),
-        find: service.find.bind(service),
-        eventBus: service.eventBus
-    }
+    VueDragulaGlobal.eventBus = service.eventBus;
+    VueDragulaGlobal.find = service.find.bind(service);
+    VueDragulaGlobal.options = service.setOptions.bind(service);
 
     vueApp.directive('dragula', ({
-        beforeMount(container: any, binding: DirectiveBinding, vnode: VNode) {
-          const bagName = vnode.props ? vnode.props['bag'] : null;
+        beforeMount(container: Element, binding: DirectiveBinding, vnode: VNode) {
+          const bagName: string | null = vnode.props ? vnode.props['bag'] : null;
 
-          if (!vnode) {
-            throw new Error('[vue3-dragula] vnode does not exist. (beforeMount)');
-          }
-
-          if (bagName !== undefined && bagName.length !== 0) {
+          if (bagName != null && bagName.trim().length !== 0) {
             name = bagName;
           }
 
-          const bag = service.find(name);
-          if (bag) {
+          let bag: Bag | null = service.find(name);
+          if (bag != null) {
             drake = bag.drake;
+
+            if(drake == null){
+              return;
+            }
+
             drake.containers.push(container);
+
+            updateModelBinding(container, binding, vnode);
+
             return;
           }
 
-          drake = dragula({
-            containers: [container]
-          })
-          service.add(name, drake);
+          drake = dragula(({ containers: [container] } as DragulaOptions));
+
+          bag = service.add(name, drake);
+          updateModelBinding(container, binding, vnode);
     
-          service.handleModels(name, drake);
+          service.handleModels(name, bag);
         },
     
-        updated(container: any, binding: DirectiveBinding, vnode: VNode, oldVnode: VNode) {
-          const newValue = vnode ? binding.value  : null;
-
-          if (!newValue) { return; }
+        updated(container: Element, binding: DirectiveBinding, vnode: VNode, oldVnode: VNode) {
+          updateModelBinding(container, binding, vnode);
+        },
+    
+        unmounted(container: Element, binding: DirectiveBinding, vnode: VNode) {
+          let unbindBagName: string = 'globalBag';
 
           const bagName: string | null = vnode.props ? vnode.props['bag'] : null;
-          if (bagName != null && bagName.length !== 0) {
-            name = bagName;
-          }
 
-          const bag = service.find(name);
-          drake = bag.drake;
-          if (!drake.models) {
-            drake.models = [];
-          }
-    
-          if (!vnode) {
-            throw new Error('[vue3-dragula] vnode does not exist. (updated)');
-          }
-          let modelContainer = service.findModelContainerByContainer(container, drake);
-    
-          if (modelContainer) {
-            modelContainer.model = newValue;
-          } else {
-            drake.models.push({
-              model: newValue,
-              container: container
-            });
-          }
-        },
-    
-        unmounted(container: any, binding: DirectiveBinding, vnode: VNode) {
-          let unbindBagName = 'globalBag';
-          const bagName: string | null = vnode .props ? vnode.props['bag'] : null;
-
-          if (!vnode) {
-            throw new Error('[vue3-dragula] vnode does not exist. (unmounted)');
-          }
-
-          if (bagName != null && bagName.length !== 0) {
+          if (bagName != null && bagName.trim().length !== 0) {
             unbindBagName = bagName;
           }
 
-          let drake = service.find(unbindBagName).drake
-          if (!drake) { return; }
-
-          let containerIndex = drake.containers.indexOf(container);
-          if (containerIndex > -1) {
-            drake.containers.splice(containerIndex, 1);
+          let bag: Bag | null = service.find(unbindBagName);
+          if (bag == null || bag.drake == null) { 
+            return; 
           }
 
-          if (drake.containers.length === 0) {
+          let containerIndex = bag.drake.containers.indexOf(container);
+          if (containerIndex > -1) {
+            bag.drake.containers.splice(containerIndex, 1);
+          }
+
+          if (bag.drake.containers.length === 0) {
             service.destroy(unbindBagName);
           }
         }
       } as Directive));
+
+      function updateModelBinding(container: Element, binding: DirectiveBinding, vnode: VNode){
+          const newValue: any[] | null = vnode ? (binding.value as any[]) : null;
+
+          if (newValue == null) { 
+            return; 
+          }
+
+          const bagName: string | null = vnode.props ? vnode.props['bag'] : null;
+          if (bagName != null && bagName.trim().length !== 0) {
+            name = bagName;
+          }
+
+          const bag = service.find(name);
+          if (bag == null) {
+            return;
+          }
+    
+          let modelContainer = service.findModelContainerByContainer(container, bag);
+    
+          if (modelContainer) {
+            modelContainer.model = newValue;
+          } else {
+            bag.models.push({
+              model: newValue,
+              container: container
+            });
+          }
+      }
 }
 
-const raf = window.requestAnimationFrame;
-const waitForTransition = raf ? function (fn: any) {
-    raf(() => {
-      raf(fn);
-    });
-  } : function (fn: any) {
-    window.setTimeout(fn, 50);
-  };
-
 class DragulaService {
-    bags: any;
-    eventBus: Emitter<any>;
-    events: any;
+  // Bag storage
+  bags: Bag[];
+  // Event bus for global access
+  eventBus: Emitter<any>;
+  events: string[];
 
   constructor () {
-    this.bags = []; // bag store
+    this.bags = [];
     this.eventBus = mitt();
     this.events = [
       'cancel',
@@ -145,126 +176,156 @@ class DragulaService {
     ];
   }
 
-  add (name: any, drake: any) {
-    let bag = this.find(name);
-    if (bag) {
-      throw new Error('Bag named: "' + name + '" already exists.')
+  add(name: string, drake: Drake): Bag {
+    let bag: Bag | null = this.find(name);
+
+    if (bag != null) {
+      throw new Error('[vue3-dragula] Bag named: "' + name + '" already exists.');
     }
-    bag = {
-      name,
-      drake
-    }
+
+    bag = new Bag(name, drake);
     this.bags.push(bag);
-    if (drake.models) {
-      this.handleModels(name, drake);
-    }
+
+    this.handleModels(name, bag);
+
     if (!bag.initEvents) {
       this.setupEvents(bag);
     }
+
     return bag;
   }
 
-  find (name: any) {
-    let bags = this.bags;
+  find(name: string): Bag | null {
+    let bags: Bag[] = this.bags;
     for (const bag of bags) {
       if (bag.name === name) {
         return bag;
       }
     }
+    return null;
   }
 
-  handleModels (name: any, drake: any) {
-    if (drake.registered) { // do not register events twice
+  handleModels(name: string, bag: Bag): void {
+    // Cancel if drak object does not exist or if it is already registert
+    if(bag.drake == null || bag.drakeRegistert){
       return;
     }
-    let dragElm: any;
-    let dragIndex: any;
-    let dropIndex: any;
-    let sourceModel: any;
-    drake.on('remove', (el: any, container: any, source: any) => {
-      if (!drake.models) {
+
+    let dragElm: Element;
+    let dragIndex: number;
+
+    // On Remove event handler
+    bag.drake.on('remove', (el: Element, container: Element, source: Element) => {
+      if (bag.drake == null) {
         return;
       }
-      sourceModel = this.findModelForContainer(source, drake);
+
+      let sourceModel: any[] = this.findModelForContainer(source, bag);
       sourceModel.splice(dragIndex, 1);
-      drake.cancel(true);
+
+      bag.drake.cancel(true);
+
+      // Emit removeModel event
       this.eventBus.emit('removeModel', [name, el, source, dragIndex]);
-    })
-    drake.on('drag', (el: any, source: any) => {
+    });
+
+    // On Drag event handler
+    bag.drake.on('drag', (el: Element, source: Element) => {
       dragElm = el;
       dragIndex = this.domIndexOf(el, source);
-    })
-    drake.on('drop', (dropElm: any, target: any, source: any) => {
-      if (!drake.models || !target) {
+    });
+
+    // On Drop event handler
+    bag.drake.on('drop', (dropElm: Element, target: Element, source: Element) => {
+      if (bag.drake == null || target == null) {
         return;
       }
-      dropIndex = this.domIndexOf(dropElm, target);
-      sourceModel = this.findModelForContainer(source, drake);
+
+      let dropIndex: number = this.domIndexOf(dropElm, target);
+      let sourceModel: any[] = this.findModelForContainer(source, bag);
 
       if (target === source) {
-        sourceModel.splice(dropIndex, 0, sourceModel.splice(dragIndex, 1)[0]);
-      } else {
+        nextTick(() => {
+          sourceModel.splice(dropIndex, 0, sourceModel.splice(dragIndex, 1)[0]);
+        });
+      } 
+      else {
         let notCopy = dragElm === dropElm;
-        let targetModel = this.findModelForContainer(target, drake);
-        let dropElmModel = notCopy ? sourceModel[dragIndex] : JSON.parse(JSON.stringify(sourceModel[dragIndex]));
+        let targetModel = this.findModelForContainer(target, bag);
+        let dropElmModel = notCopy ? sourceModel[dragIndex] : this.cloneObject(sourceModel[dragIndex]);
 
         if (notCopy) {
-          waitForTransition(() => {
+          nextTick(() => {
             sourceModel.splice(dragIndex, 1);
-          })
+          });
         }
-        targetModel.splice(dropIndex, 0, dropElmModel);
-        drake.cancel(true);
+
+        nextTick(() => {
+          targetModel.splice(dropIndex, 0, dropElmModel);
+        });
       }
+
+      bag.drake.cancel(true);
+
+      // Emit dropModel event 
       this.eventBus.emit('dropModel', [name, dropElm, target, source, dropIndex]);
-    })
-    drake.registered = true;
+    });
+
+    // Set registration flag
+    bag.drakeRegistert = true;
   }
 
-  destroy (name: any) {
-    let bag = this.find(name);
+  destroy(name: string): void {
+    let bag: Bag | null = this.find(name);
 
-    if (!bag) { return; }
+    if (bag == null){ 
+      return; 
+    }
 
-    let bagIndex = this.bags.indexOf(bag);
+    let bagIndex: number = this.bags.indexOf(bag);
     this.bags.splice(bagIndex, 1);
-    bag.drake.destroy();
+
+    bag.destroy();
   }
 
-  setOptions (name: any, options: any) {
-    let bag = this.add(name, dragula(options));
-    this.handleModels(name, bag.drake);
+  setOptions(name: string, options: DragulaOptions): void {
+    let bag: Bag = this.add(name, dragula(options));
+    if(bag.drake != null){
+      this.handleModels(name, bag);
+    }
   }
 
-  setupEvents (bag: any) {
+  setupEvents(bag: Bag): void {
     bag.initEvents = true;
-    let _this = this;
+    let _this: this = this;
 
     let emitter = (type: any) => {
       function replicate () {
-        let args = Array.prototype.slice.call(arguments);
+        let args: any[] = Array.prototype.slice.call(arguments);
         _this.eventBus.emit(type, [bag.name].concat(args));
       }
 
-      bag.drake.on(type, replicate);
+      if(bag.drake != null){
+        bag.drake.on(type, replicate);
+      }
     }
 
     this.events.forEach(emitter);
   }
 
-  domIndexOf (child: any, parent: any) {
+  domIndexOf(child: Element, parent: Element): number {
     return Array.prototype.indexOf.call(parent.children, child);
   }
 
-  findModelForContainer (container: any, drake: any) {
-    return this.findModelContainerByContainer(container, drake)?.model;
+  findModelForContainer(container: Element, bag: Bag): any {
+    return this.findModelContainerByContainer(container, bag)?.model;
   }
 
-  findModelContainerByContainer (container: any, drake: any) {
-    if (!drake.models) {
-      return;
-    }
+  findModelContainerByContainer(container: Element, bag: Bag): any {
+    return bag.models.find((model: any) => model.container === container);
+  }
 
-    return drake.models.find((model: any) => model.container === container);
+  cloneObject(object: any): any{
+    return JSON.parse(JSON.stringify(object))
   }
 }
